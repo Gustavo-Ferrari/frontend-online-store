04fe73adf330849acd086d77c8206630caa1abff
Gustavo-Ferrari <gustavod.ferrari@gmail.com>
2022-05-05 11:20:00 -0300

    minor changes

--- src/components/CategoriesSidebar.jsx
+++ src/components/CategoriesSidebar.jsx
@@ -20,7 +20,7 @@ class CategoriesSidebar extends Component {
     const { categoriesArray } = this.props;
     return (
       <div className="sideCard">
-        <p>CategoriesSidebar</p>
+        <p>Categorias</p>
         <ul>
           { categoriesArray.map((category) => (
             <li key={ category.id }>
